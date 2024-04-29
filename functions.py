4d3120ed39d2ed4c9c5d83fe5e21be7213c42599
ema-malinauskaite <ema.malinauskaite@gmail.com>
2024-04-29 13:42:11 +0300

    minor fixes

--- functions.py
+++ functions.py
@@ -1,24 +1,25 @@
 import csv
 
+
 def getListFromCSV(file):
     with open(file) as file:
         csvReader = csv.DictReader(file, delimiter=',')
         sampleData = list(csvReader)
     return sampleData
 
+
 def zeroInEmpty(dict):
     allValues = set()
     for a in dict:
         for b in dict[a]:
             allValues.update(list(dict[a]))
-
     allValues = list(allValues)
     for a in dict:
         for i in allValues:
             if i not in dict[a]:
                 dict[a][i] = 0.0
-
     return dict
+
 
 def createEmpty(dict, levelName, emptyValue):
     if levelName not in dict:
